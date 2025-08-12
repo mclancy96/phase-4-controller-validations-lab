aab0756cc7249d2968ce71c294b603769eec53d7
Mike <mclancy96@gmail.com>
2025-08-12 09:26:47 -0400

    Error on failed validation and handle error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,24 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def show
     post = Post.find(params[:id])
-    
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
   end
 
   private
 
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors }, status: :unprocessable_entity
+  end
+
   def post_params
     params.permit(:category, :content, :title)
   end
-
 end
